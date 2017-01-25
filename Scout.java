4784e33bb94bc17beaa66e34b6e98d5a1af55050
Eric Chen <onenation98@dhcp-18-111-18-162.dyn.mit.edu>
2017-01-25 00:41:21 -0500

    scout finds nearest bullet tree and moves towards it to harvest bullets

--- Scout.java
+++ Scout.java
@@ -1,6 +1,6 @@
 package battlecode2017;
 
-import battlecode.common.RobotController;
+import battlecode.common.*;
 
 public class Scout extends AbstractBot {
 
@@ -8,7 +8,20 @@ public class Scout extends AbstractBot {
 		super(rc);
 		// TODO Auto-generated constructor stub
 	}
-	public void run() {
-	}
+	public void run() throws GameActionException {
+		trees.update();
+		MapLocation myLocation = rc.getLocation();
 
+		TreeInfo closest_neutral_tree = trees.getClosestTree(Team.NEUTRAL); //get closest neutral TreeInfo object
+		if (closest_neutral_tree.containedBullets > 0) { //if said tree has bullets, then move towards it
+			if (rc.canInteractWithTree(closest_neutral_tree.getLocation()) && rc.canShake()) {
+				rc.shake(closest_neutral_tree.getLocation());
+			} else {
+				Direction dir = myLocation.directionTo(closest_neutral_tree.getLocation());
+				if (rc.canMove(dir)) {
+					rc.move(dir);
+				}
+			}
+		}
+	}
 }
