0f50ef1f05b9547f2c2a058e9bd4d2844ad46077
Gruber <gruber@mit.edu>
2017-01-27 00:15:03 -0500

    major upgrades to scout and lumberjack logic

--- Lumberjack.java
+++ Lumberjack.java
@@ -2,12 +2,7 @@ package battlecode2017;
 
 import java.util.List;
 
-import battlecode.common.GameActionException;
-import battlecode.common.MapLocation;
-import battlecode.common.RobotController;
-import battlecode.common.RobotType;
-import battlecode.common.Team;
-import battlecode.common.TreeInfo;
+import battlecode.common.*;
 
 public class Lumberjack extends AbstractBot {
 
@@ -19,6 +14,7 @@ public class Lumberjack extends AbstractBot {
 		trees.update();
 		bots.update();
 		dodge();
+		chaseAndStrikeEnemies();
 		strikeOrChop();
 		moveTowardsOrWander(nearestEnemyBotOrTreeOrNeutralTree());
     }
@@ -28,8 +24,62 @@ public class Lumberjack extends AbstractBot {
 			rc.strike();
 		}
 	}
-	
-	public void strikeOrChop() throws GameActionException{
+
+	/**This function has the lumberjack strike an enemy if it's in range or otherwise
+	 *  move towards the nearest enemy if it is the closest friendly lumberjack
+	 *  or otherwise make sure it won't get hit by the nearest friendly lumberjack.
+	 *  This function will hit friendly bots!!!! I didn't bother fixing that and also thought it was worth it.
+	 *  This function may not be perfect, but it will likely do decently well.
+	 * @return true if we moved or struck, false otherwise
+	 * @throws GameActionException
+	 */
+	public boolean chaseAndStrikeEnemies() throws GameActionException {
+		//Get info I'll need
+		RobotInfo nearestEBot = bots.getClosestbot(team.opponent());
+		List<RobotInfo> FBots = bots.getBots(team);
+
+		//if we found an enemy bot
+		if (nearestEBot!=null) {
+			//check if any friendly lumberjacks are closer
+			boolean closerLumb = false;
+			RobotInfo aforementionedLumb = null;
+			for (RobotInfo ri: FBots) {
+				if (ri.getType().equals(RobotType.LUMBERJACK)
+						&& ri.location.distanceTo(nearestEBot.location)
+						< rc.getLocation().distanceTo(nearestEBot.location)) {
+					closerLumb = true;
+					aforementionedLumb = ri;
+					break;
+				}
+			}
+			//if there are no closer lumberjacks, go in for the kill
+			if (!closerLumb) {
+				if (rc.getLocation().distanceTo(nearestEBot.location) - nearestEBot.getType().bodyRadius < GameConstants.LUMBERJACK_STRIKE_RADIUS) {
+					//CAN strike friendlies!! lumberjacks know to avoid, but other friendlies do not, word of warning
+					strike();
+					return true;
+				} else {
+					return moveTo(nearestEBot.location);
+				}
+			}
+			//if there is a closer lumberjack, make sure you keep your distance so you don't get chopped yourself
+			else {
+				//calculate where our closer lumberjack is gonna go
+				MapLocation attackSpot = nearestEBot.location.add(nearestEBot.location.directionTo(aforementionedLumb.location), GameConstants.LUMBERJACK_STRIKE_RADIUS);
+				//make sure we're out of the strike radius from that point. if not, get away further.
+				if (rc.getLocation().distanceTo(attackSpot) - rc.getType().bodyRadius <= GameConstants.LUMBERJACK_STRIKE_RADIUS) {
+					return tryMove(rc.getLocation().directionTo(attackSpot).rotateLeftDegrees((float) 180));
+				} else return false; //If we're out of the way, just return false
+			}
+		}
+		return false; //didn't see anyone
+	}
+
+	public boolean strikeOrChop() throws GameActionException{
+		//save some time in case already did something
+		if (!rc.canStrike()) return false;
+
+		//carry on
 		boolean shouldStrike;
 		List<TreeInfo> enemyCanInteract = trees.getTreesWithinInteract(team.opponent());
 		List<TreeInfo> neutralCanInteract = trees.getTreesWithinInteract(team.NEUTRAL);
@@ -55,13 +105,16 @@ public class Lumberjack extends AbstractBot {
 			chop(enemyCanInteract.get(0).ID);
 		} else if (neutralTreeCount > 0){
 			chop(neutralCanInteract.get(0).ID);
-		} 
+		}
+		return shouldStrike;
 	}
 	public void chop(int id) throws GameActionException{
 		if (rc.canChop(id)){
 			rc.chop(id);
 		}
+
 	}
+
 	
 	
 	
